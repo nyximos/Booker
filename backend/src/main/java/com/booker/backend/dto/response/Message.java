7048e24e5949e1af6f639eac2085e44e131ed693
nyximos <nyximos@gmail.com>
2022-09-28 23:18:00 +0900

    add:response

---
+++ backend/src/main/java/com/booker/backend/dto/response/Message.java
@@ -0,0 +1,21 @@
+package com.booker.backend.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Message<T> {
+    private StatusEnum statusEnum;
+    private String message;
+    private T data;
+
+    @Builder
+    public Message(StatusEnum statusEnum, String message, T data) {
+        this.statusEnum = statusEnum;
+        this.message = message;
+        this.data = data;
+    }
+}
